0738b2fa98857bc01405ea11f6767fb827211559
pajelonek <pawel.jelonek.krakow@gmail.com>
2020-03-17 19:33:23 +0100

    Initial spring-boot commit.

---
+++ src/main/java/com/licencjat/filesynchronizer/client/filesynchronizerclient/FileSynchronizerClientApplication.java
@@ -0,0 +1,13 @@
+package com.licencjat.filesynchronizer.client.filesynchronizerclient;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FileSynchronizerClientApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FileSynchronizerClientApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/licencjat/filesynchronizer/client/filesynchronizerclient/FileSynchronizerClientApplicationTests.java
@@ -0,0 +1,13 @@
+package com.licencjat.filesynchronizer.client.filesynchronizerclient;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FileSynchronizerClientApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
