01210836f6542c26ac9cb95136ce67364aa4039b
pajelonek <pawel.jelonek.krakow@gmail.com>
2020-06-26 14:21:35 +0200

    Refactoring rsyncprovider.

--- src/main/java/com/licencjat/filesynchronizer/client/rsync/RSyncFileUpdaterProvider.java
+++ src/main/java/com/licencjat/filesynchronizer/client/rsync/RSyncFileUpdaterProvider.java
@@ -7,7 +7,6 @@ import com.licencjat.filesynchronizer.client.config.RestTemplateConfig;
 import com.licencjat.filesynchronizer.client.model.UpdateFile;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.devtools.filewatch.ChangedFile;
 import org.springframework.stereotype.Component;
@@ -22,11 +21,9 @@ import java.util.stream.Stream;
 @ContextConfiguration(classes = {RestTemplateConfig.class, HttpClientConfig.class})
 public class RSyncFileUpdaterProvider {
 
-    @Autowired
-    RSyncFileUpdaterExecutor rSyncFileUpdaterExecutor;
+    final RSyncFileUpdaterExecutor rSyncFileUpdaterExecutor;
 
-    @Autowired
-    FileUpdaterRequestSender fileUpdaterRequestSender;
+    final FileUpdaterRequestSender fileUpdaterRequestSender;
 
     @Value("${user.local.directory}")
     private String userLocalDirectory;
@@ -38,72 +35,49 @@ public class RSyncFileUpdaterProvider {
 
     Logger logger = LoggerFactory.getLogger(RSyncFileUpdaterExecutor.class);
 
+    public RSyncFileUpdaterProvider(RSyncFileUpdaterExecutor rSyncFileUpdaterExecutor, FileUpdaterRequestSender fileUpdaterRequestSender) {
+        this.rSyncFileUpdaterExecutor = rSyncFileUpdaterExecutor;
+        this.fileUpdaterRequestSender = fileUpdaterRequestSender;
+    }
+
     public void processComparing(List<UpdateFile> serverFileList, List<UpdateFile> clientFileList) {
         logger.info("Starting comparing files with server");
-//        updateExistingFilesOnServer(serverFileList, clientFileList);
-//        updateExistingFilesOnClient(serverFileList, clientFileList);
 
-        updateDirectoryOnClient(serverFileList, clientFileList);
-//        updateDirectoryOnServer(serverFileList, clientFileList);
+        List<UpdateFile> filesToUpdateOnClientList = getExistingFilesToUpdate(serverFileList, clientFileList);
+        logger.info("Found {} files to update on client", filesToUpdateOnClientList.size());
 
-//        addNewFilesFromClientToServer(serverFileList, clientFileList);
-//        addNewFilesFromServerToClient(serverFileList, clientFileList);
-    }
-    public void processForServer(List<UpdateFile> clientFileList) {
-        List<UpdateFile> filesToRsync = clientFileList.stream()
-                .filter(file -> file.getAction().equals("MODIFY") || file.getAction().equals("ADD"))
-                .collect(Collectors.toList());
+        List<UpdateFile> filesToUploadOnClientList = getNewFilesToUploadOnClient(serverFileList, clientFileList);
+        logger.info("Found {} new files to upload on client", filesToUploadOnClientList.size());
 
-        logger.info("Found {} files to update on server: {}", filesToRsync.size(), filesToRsync.toString());
-        processOnServer(filesToRsync);
+        List<UpdateFile> filesToUploadFromServerList = Stream.of(filesToUpdateOnClientList, filesToUploadOnClientList).flatMap(List::stream).collect(Collectors.toList());
 
-        List<UpdateFile> fileToRemoveOnServer = clientFileList.stream()
-                .filter(file -> file.getAction().equals("DELETE"))
-                .collect(Collectors.toList());
-        logger.info("Found {} files to remove on server: {}", fileToRemoveOnServer.size(), fileToRemoveOnServer.toString());
-        fileUpdaterRequestSender.removeFilesOnServer(fileToRemoveOnServer);
-    }
+        processOnClient(filesToUploadFromServerList);
 
- /*       private void updateDirectoryOnServer(List<FileRQList> serverFileList, List<FileRQList> clientFileList) {
-        List<FileRQList> fileToUpdateOnServer = getExistingFilesToUpdate(clientFileList, serverFileList);
+        List<UpdateFile> filesToDeleteOnClientList = getFilesToDeleteOnClient(serverFileList, clientFileList);
+        logger.info("Found {} new files to delete on client", filesToDeleteOnClientList.size());
 
-        logger.info("Found {} files to update on server: {}", fileToUpdateOnServer.size(), fileToUpdateOnServer.toString());
-
-        List<FileRQList> filesNotFoundOnServer = getNewFilesToUploadOnServer(serverFileList, clientFileList);
-
-        logger.info("Found {} new files to upload to server: {}", filesNotFoundOnServer.size(), filesNotFoundOnServer.toString());
-
-        processOnServer(fileToUpdateOnServer);
-    }*/
-
-   /* private List<FileRQList> getNewFilesToUploadOnServer(List<FileRQList> serverFileList, List<FileRQList> clientFileList) {
-        return clientFileList.stream()
-                .filter(file -> !isFileOnServer(file, serverFileList))
-                .collect(Collectors.toList());
-    }*/
-
-    private void updateDirectoryOnClient(List<UpdateFile> serverFileList, List<UpdateFile> clientFileList) {
-        List<UpdateFile> existingFilesToUpdateOnClient = getExistingFilesToUpdate(serverFileList, clientFileList);
-
-        logger.info("Found {} files to update on client", existingFilesToUpdateOnClient.size());
-
-        List<UpdateFile> newFilesToUploadOnClient = getNewFilesToUploadOnClient(serverFileList, clientFileList);
-
-        logger.info("Found {} new files to upload on client", newFilesToUploadOnClient.size());
-
-        List<UpdateFile> fileToUpdateOnClient = Stream.of(existingFilesToUpdateOnClient, newFilesToUploadOnClient).flatMap(List::stream).collect(Collectors.toList());
-
-        processOnClient(fileToUpdateOnClient);
-
-        List<UpdateFile> filesToDeleteOnClient = getFilesToDeleteOnClient(serverFileList, clientFileList);
-
-        logger.info("Found {} new files to delete on client", filesToDeleteOnClient.size());
-        deleteOnClient(filesToDeleteOnClient);
+        deleteOnClient(filesToDeleteOnClientList);
 
         updateModificationDateOnDirectories(userLocalDirectory);
     }
-    //TODO Do I need this functionality?
-    private void updateModificationDateOnDirectories(String userLocalDirectory){
+
+    public void processForServer(List<UpdateFile> clientFileList) {
+        List<UpdateFile> filesToSendToServerList = clientFileList.stream()
+                .filter(file -> file.getAction().equals("MODIFY") || file.getAction().equals("ADD"))
+                .collect(Collectors.toList());
+
+        logger.info("Found {} files to update on server: {}", filesToSendToServerList.size(), filesToSendToServerList.toString());
+        processOnServer(filesToSendToServerList);
+
+        List<UpdateFile> filesToRemoveOnServerList = clientFileList.stream()
+                .filter(file -> file.getAction().equals("DELETE"))
+                .collect(Collectors.toList());
+        logger.info("Found {} files to remove on server: {}", filesToRemoveOnServerList.size(), filesToRemoveOnServerList.toString());
+        fileUpdaterRequestSender.removeFilesOnServer(filesToRemoveOnServerList);
+    }
+
+    //TODO put into diff branch after everything
+    private void updateModificationDateOnDirectories(String userLocalDirectory) {
 //        try {
 //            List<String> directoryPathsList = Files.walk(Paths.get(userLocalDirectory+"\\"))
 //                    .filter(Files::isDirectory)
@@ -122,128 +96,87 @@ public class RSyncFileUpdaterProvider {
 //        }
     }
 
-    public void deleteOnClient(List<UpdateFile> filesToDeleteOnClient) {
-        for (UpdateFile fileRQ : filesToDeleteOnClient) {
-            logger.info("Removing file: " + fileRQ.getFilePath());
-            File file = new File(userLocalDirectory + fileRQ.getFilePath());
+    public void deleteOnClient(List<UpdateFile> filesToDeleteOnClientList) {
+        for (UpdateFile updateFile : filesToDeleteOnClientList) {
+            logger.info("Removing file: " + updateFile.getFilePath());
+            File file = new File(userLocalDirectory + updateFile.getFilePath());
             if (file.exists() && file.delete()) {
-                logger.info("Successfully deleted file {} on server: " + fileRQ.getFilePath());
-            } else throw new Error("Could not find file on server");
+                logger.info("Successfully deleted file {} on client: " + updateFile.getFilePath());
+            } else throw new Error("Could not find or delete file on client");
         }
     }
 
-    private List<UpdateFile> getFilesToDeleteOnClient(List<UpdateFile> serverFileList, List<UpdateFile> clientFileList) {
+    public List<UpdateFile> getFilesToDeleteOnClient(List<UpdateFile> serverFileList, List<UpdateFile> clientFileList) {
         List<String> serverFileNames = serverFileList.stream()
                 .map(UpdateFile::getFilePath)
                 .collect(Collectors.toList());
 
         return clientFileList.stream()
-                .filter(file -> !serverFileNames.contains(file.getFilePath()))
+                .filter(clientFile -> !serverFileNames.contains(clientFile.getFilePath()))
                 .collect(Collectors.toList());
     }
 
-    private List<UpdateFile> getNewFilesToUploadOnClient(List<UpdateFile> serverFileList, List<UpdateFile> clientFileList) {
-        List<String> filesNameListFromClient = clientFileList.stream()
+    public List<UpdateFile> getNewFilesToUploadOnClient(List<UpdateFile> serverFileList, List<UpdateFile> clientFileList) {
+        List<String> filesFromClientList = clientFileList.stream()
                 .map(UpdateFile::getFilePath)
                 .collect(Collectors.toList());
 
         return serverFileList.stream()
-                .filter(file -> validateIfUploadFile(file, filesNameListFromClient))
+                .filter(serverFile -> isFileOnClient(serverFile, filesFromClientList))
                 .collect(Collectors.toList());
     }
 
-    private List<UpdateFile> getExistingFilesToUpdate(List<UpdateFile> serverFileList, List<UpdateFile> clientFileList) {
+    public List<UpdateFile> getExistingFilesToUpdate(List<UpdateFile> serverFileList, List<UpdateFile> clientFileList) {
         return serverFileList.stream()
                 .filter(file -> validateIfUpdateFile(file, clientFileList))
                 .collect(Collectors.toList());
     }
 
-    private boolean validateIfUploadFile(UpdateFile file, List<String> clientFileList) {
+    private boolean isFileOnClient(UpdateFile file, List<String> clientFileList) {
         return !clientFileList.contains(file.getFilePath());
     }
 
-//    public void updateExistingFilesOnClient(List<FileRQList> serverFileList, List<FileRQList> clientFileList) {
-//        List<FileRQList> fileToUpdateOnClient = serverFileList.stream()
-//                .filter(file -> validateIfUpdateFile(file, clientFileList))
-//                .collect(Collectors.toList());
-//
-//        logger.info("Found {} files to update on client", fileToUpdateOnClient.size());
-//        processOnClient(fileToUpdateOnClient);
-//    }
-//
-//    public void addNewFilesFromServerToClient(List<FileRQList> serverFileList, List<FileRQList> clientFileList) {
-//        List<String> fileNamesFromClient = clientFileList.stream()
-//                .map(FileRQList::getFilePath)
-//                .collect(Collectors.toList());
-//
-//        List<FileRQList> fileToUpdateOnClient = serverFileList.stream()
-//                .filter(file -> !fileNamesFromClient.contains(file.getFilePath()))
-//                .collect(Collectors.toList());
-//        logger.info("Found {} files to upload on client", fileToUpdateOnClient.size());
-//        processOnClient(fileToUpdateOnClient);
-//    }
-
+    //todo opisz
     public void processOnClient(List<UpdateFile> fileToUpdateOnClient) {
         fileToUpdateOnClient.stream()
                 .collect(Collectors.groupingBy(fileToUpdate -> fileToUpdate.getFilePath().substring(0, fileToUpdate.getFilePath().lastIndexOf('\\'))))
                 .forEach((prefixOfPath, fileRQ) -> modifyFileOnClient(fileRQ, prefixOfPath));
     }
 
-    private void modifyFileOnClient(List<UpdateFile> fileRQ, String prefixOfPath) {
-        List<String> sources = fileRQ.stream()
+    //todo change hostName to sshHostName/sshServerName
+    private void modifyFileOnClient(List<UpdateFile> updateFileList, String prefixOfPath) {
+        List<String> sourcesList = updateFileList.stream()
                 .map(UpdateFile::getFilePath)
-                .map(FilePath -> hostName+ ":"+ remoteMainFolder + FilePath)
+                .map(filePath -> hostName + ":" + remoteMainFolder + filePath)
                 .collect(Collectors.toList());
 
-        logger.info("Modifying {} file/files on client", sources.toString());
+        logger.info("Modifying {} file/files on client", sourcesList.toString());
         rSyncFileUpdaterExecutor
-                .setSources(sources)
+                .setSources(sourcesList)
                 .setDestination(userLocalDirectory + prefixOfPath + "\\")
                 .execute();
 
-        updateFileModificationDateOnClient(fileRQ);
+        updateFileModificationDateOnClient(updateFileList);
     }
 
-    private void updateFileModificationDateOnClient(List<UpdateFile> updateFile) {
-        for (UpdateFile fileRQ : updateFile) {
-            logger.info("Changing modification date for file: " + fileRQ.getFilePath());
-            File file = new File(userLocalDirectory + fileRQ.getFilePath());
-            if (file.exists() && file.setLastModified(Long.parseLong(fileRQ.getLastModified()))) {
-                logger.info("Successfully modified date for file: " + fileRQ.getFilePath());
-//                validateDirectoryModificationDate(fileRQ);
+    public void updateFileModificationDateOnClient(List<UpdateFile> updateFileList) {
+        for (UpdateFile updateFile : updateFileList) {
+            logger.info("Changing modification date for file: " + updateFile.getFilePath());
+            File file = new File(userLocalDirectory + updateFile.getFilePath());
+            if (file.exists() && file.setLastModified(Long.parseLong(updateFile.getLastModified()))) {
+                logger.info("Successfully modified date for file: " + updateFile.getFilePath());
             } else throw new Error("Could not find file on client");
         }
     }
 
-
-
-//    private void updateExistingFilesOnServer(List<FileRQList> serverFileList, List<FileRQList> clientFileList) {
-//        List<FileRQList> fileToUpdateOnServer = clientFileList.stream()
-//                .filter(file -> validateIfUpdateFile(file, serverFileList))
-//                .collect(Collectors.toList());
-//
-//        logger.info("Found {} files to update on server", fileToUpdateOnServer.size());
-//        processOnServer(fileToUpdateOnServer);
-//    }
-
-//    private void addNewFilesFromClientToServer(List<FileRQList> serverFileList, List<FileRQList> clientFileList) {
-//        List<FileRQList> filesNotFoundOnServer = clientFileList.stream()
-//                .filter(file -> !isFileOnServer(file, serverFileList))
-//                .collect(Collectors.toList());
-//
-//        logger.info("Found {} file/files to upload to server", filesNotFoundOnServer.size());
-//        processOnServer(filesNotFoundOnServer);
-//    }
-
-    public void processOnServer(List<UpdateFile> fileToUpdateOnServer) {
-        fileToUpdateOnServer.stream()
+    public void processOnServer(List<UpdateFile> filesToSendToServerList) {
+        filesToSendToServerList.stream()
                 .collect(Collectors.groupingBy(fileToUpdate -> fileToUpdate.getFilePath().substring(0, fileToUpdate.getFilePath().lastIndexOf('\\'))))
-                .forEach((prefixOfPath, fileRQ) -> modifyFileOnServer(fileRQ, prefixOfPath));
+                .forEach((prefixOfPath, file) -> modifyFileOnServer(file, prefixOfPath));
     }
 
-
-    private void modifyFileOnServer(List<UpdateFile> fileRQ, String prefixOfPath) {
-        List<String> sources = fileRQ.stream()
+    private void modifyFileOnServer(List<UpdateFile> updateFileList, String prefixOfPath) {
+        List<String> sources = updateFileList.stream()
                 .map(UpdateFile::getFilePath)
                 .map(FilePath -> userLocalDirectory + FilePath)
                 .collect(Collectors.toList());
@@ -254,14 +187,14 @@ public class RSyncFileUpdaterProvider {
                 .setDestination(hostName + ":" + remoteMainFolder + prefixOfPath + "\\")
                 .execute();
 
-        updateFileModificationDateOnServer(fileRQ);
+        updateFileModificationDateOnServer(updateFileList);
     }
 
-    public List<UpdateFile> mapToFileRQList(Map<String, ChangedFile.Type> updatedFiles) {
+    public List<UpdateFile> mapToUpdateFileList(Map<String, ChangedFile.Type> updatedFiles) {
         List<UpdateFile> listToUpdate = new ArrayList<>();
-        for (Map.Entry<String,ChangedFile.Type> entry : updatedFiles.entrySet()) {
+        for (Map.Entry<String, ChangedFile.Type> entry : updatedFiles.entrySet()) {
             UpdateFile updateFile = new UpdateFile();
-            updateFile.setFilePath(entry.getKey().replace(userLocalDirectory,""));
+            updateFile.setFilePath(entry.getKey().replace(userLocalDirectory, ""));
             File file = new File(entry.getKey());
             updateFile.setLastModified(String.valueOf(file.lastModified()));
             updateFile.setAction(entry.getValue().toString());
@@ -270,24 +203,17 @@ public class RSyncFileUpdaterProvider {
         return listToUpdate;
     }
 
-
     private void updateFileModificationDateOnServer(List<UpdateFile> updateFile) {
         fileUpdaterRequestSender.updateDateModification(updateFile);
     }
 
-    private boolean validateIfUpdateFile(UpdateFile clientFile, List<UpdateFile> serverFileList) {
-        Optional<UpdateFile> fileOnServer = serverFileList.stream()
-                .filter(serverFile -> serverFile.getFilePath().equals(clientFile.getFilePath()))
+    //todo think about != instead of <
+    private boolean validateIfUpdateFile(UpdateFile serverFile, List<UpdateFile> clientFileList) {
+        Optional<UpdateFile> fileOnServer = clientFileList.stream()
+                .filter(clientFile -> clientFile.getFilePath().equals(serverFile.getFilePath()))
                 .findAny();
-        return fileOnServer.filter(fileRQList -> Long.parseLong(fileRQList.getLastModified()) < Long.parseLong(clientFile.getLastModified())).isPresent();
+        return fileOnServer.filter(fileRQList -> Long.parseLong(fileRQList.getLastModified()) < Long.parseLong(serverFile.getLastModified())).isPresent();
     }
-
-//    private boolean isFileOnServer(FileRQList filePath, List<FileRQList> serverFileList) {
-//        return serverFileList.stream()
-//                .map(FileRQList::getFilePath)
-//                .map(serverPath -> serverPath.replace(remoteMainFolder, ""))
-//                .anyMatch(fileOnServer -> fileOnServer.equals(filePath.getFilePath()));
-//    }
 
     public RSyncFileUpdaterProvider setRemoteMainFolder(String remoteMainFolder) {
         this.remoteMainFolder = remoteMainFolder;
