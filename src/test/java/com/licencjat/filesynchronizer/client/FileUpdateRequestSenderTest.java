ac89d9874be95b679cc28ad0c1e155a4c3c225be
pajelonek <pawel.jelonek.krakow@gmail.com>
2020-06-26 14:18:29 +0200

    Adding test classes for all components.

---
+++ src/test/java/com/licencjat/filesynchronizer/client/FileUpdateRequestSenderTest.java
@@ -0,0 +1,70 @@
+package com.licencjat.filesynchronizer.client;
+
+import com.licencjat.filesynchronizer.client.components.FileUpdaterRequestSender;
+import com.licencjat.filesynchronizer.client.model.UpdateFile;
+import com.licencjat.filesynchronizer.client.model.UpdateFilesRQ;
+import org.junit.jupiter.api.Order;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.http.HttpEntity;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Objects;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+@SpringBootTest(properties = "environment=TEST")
+public class FileUpdateRequestSenderTest {
+
+    @Autowired
+    FileUpdaterRequestSender fileUpdateRequestSender;
+
+    private List<String> setOne;
+
+    @Test
+    @Order(1)
+    void contextLoads(){
+        assertThat(fileUpdateRequestSender).isNotNull();
+    }
+
+    @Test
+    void createUpdateFilesRequestTest(){
+        //when
+        List<UpdateFile> updateFileList = createUpdateFileList();
+
+        //given
+        HttpEntity<UpdateFilesRQ> updateFileRQEntity = fileUpdateRequestSender.createUpdateFilesRequest(updateFileList);
+
+        //then
+        assertThat(Objects.requireNonNull(updateFileRQEntity.getBody()).getMainFolder()).isNotNull();
+        assertThat(updateFileRQEntity.getBody().getHost()).isNotNull();
+        assertThat(updateFileRQEntity.getBody().getName()).isNotNull();
+        assertThat(updateFileRQEntity.getBody().getUpdateFile().size()).isEqualTo(setOne.size());
+    }
+
+
+    private List<UpdateFile> createUpdateFileList() {
+        createSetOne();
+        List<UpdateFile> updateFileList = new ArrayList<>();
+
+        for(String testFile : setOne){
+            UpdateFile updateFile = new UpdateFile();
+            updateFile.setFilePath(testFile);
+            updateFile.setLastModified("1");
+            updateFile.setAction("TEST");
+            updateFileList.add(updateFile);
+        }
+        return updateFileList;
+    }
+
+    private void createSetOne() {
+        setOne = new ArrayList<>();
+        setOne.add("/testDirectory/fileOne.txt");
+        setOne.add("/testDirectory/fileTwo.txt");
+        setOne.add("/testDirectory/SubDirectory/FileOne.txt");
+    }
+
+}
+
---
+++ src/test/java/com/licencjat/filesynchronizer/client/RsyncFileUpdaterExecutorTest.java
@@ -0,0 +1,129 @@
+package com.licencjat.filesynchronizer.client;
+
+import com.licencjat.filesynchronizer.client.rsync.RSyncFileUpdaterExecutor;
+import org.apache.commons.io.FileUtils;
+import org.junit.jupiter.api.Order;
+import org.junit.jupiter.api.Test;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.boot.test.context.SpringBootTest;
+
+import java.io.File;
+import java.io.IOException;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.stream.Collectors;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+@SpringBootTest(properties = {"environment=TEST", "rsync.remote.shell="})
+public class RsyncFileUpdaterExecutorTest {
+
+    @Autowired
+    RSyncFileUpdaterExecutor rSyncFileUpdaterExecutor;
+
+    @Value("${user.local.directory}")
+    private String userLocalDirectory;
+
+    Logger logger = LoggerFactory.getLogger(RsyncFileUpdaterExecutorTest.class);
+
+    private List<String> setOne;
+    private List<String> setOneDirectoryList;
+    private String testDirectory = "/testDirectory";
+    private String rsyncDirectory = testDirectory + "/rsyncDirectory";
+
+    @Test
+    @Order(1)
+    void contextLoads(){
+        assertThat(rSyncFileUpdaterExecutor).isNotNull();
+    }
+
+    @Test
+    void testExecution(){
+        //when
+        createResourcesFilesSetOne();
+        List<String> sourcesList = createSourcesList(setOne);
+        String destinationDirectory = userLocalDirectory.replace("\\", "/") + rsyncDirectory + "/";
+
+        //given
+        rSyncFileUpdaterExecutor
+                .setSources(sourcesList)
+                .setDestination(destinationDirectory)
+                .execute();
+
+        //then
+        validateAreFilesCopied(sourcesList);
+        deleteTestFiles(setOne);
+    }
+
+    private void deleteTestFiles(List<String> setOne) {
+        for(String path : setOne){
+            String fixedPosix = path.replace(testDirectory.replace("/","\\"), rsyncDirectory.replace("/","\\"));
+            String correctNewPath = userLocalDirectory + fixedPosix;
+
+            File file = new File(correctNewPath);
+            file.deleteOnExit();
+        }
+    }
+
+    private void validateAreFilesCopied(List<String> sourcesList) {
+        for(String source : sourcesList){
+            File file = new File(source);
+            assertThat(file.exists());
+            file.deleteOnExit();
+        }
+
+    }
+
+    private List<String> createSourcesList(List<String> setOne) {
+        return setOne.stream()
+                .map(file -> userLocalDirectory + file)
+                .collect(Collectors.toList());
+    }
+
+
+    private void createResourcesFilesSetOne() {
+        createSetOne();
+        createSetOneDirectoryList();
+        createDirectory(userLocalDirectory + testDirectory);
+        for (String directory : setOneDirectoryList) {
+            createDirectory(userLocalDirectory + directory);
+        }
+        try {
+            for (String filePath : setOne) {
+                File file = new File(userLocalDirectory + filePath);
+                FileUtils.touch(file);
+                file.deleteOnExit();
+            }
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+
+    private void createSetOneDirectoryList() {
+        setOneDirectoryList = new ArrayList<>();
+        setOneDirectoryList.add(rsyncDirectory);
+    }
+
+    private void createSetOne() {
+        setOne = new ArrayList<>();
+        setOne.add("\\testDirectory\\fileOne.txt");
+        setOne.add("\\testDirectory\\fileTwo.txt");
+        setOne.add("\\testDirectory\\fileThree.txt");
+        setOne.add("\\testDirectory\\fileFour.txt");
+        setOne.add("\\testDirectory\\fileFive.txt");
+    }
+
+    void createDirectory(String path) {
+        File directory = new File(path);
+        if (!directory.exists()) {
+            if (directory.mkdir())
+                logger.info("Successfully created test directory '{}'", path);
+            else throw new Error("Could not create directory");
+            directory.deleteOnExit();
+        } else logger.info("Test directory '{}' already exists", path);
+
+    }
+}
